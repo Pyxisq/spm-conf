58ce9c27f887b0d18e23bbbc6677afcebb567dee
Dulat <bbdulat@gmail.com>
2021-03-20 13:41:28 +0600

    First commit

---
+++ Sources/TestPacket/File.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by SPM on 20.03.2021.
+//
+
+import SwiftUI
+
+public struct CustomButton: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        Button("Hello world") {
+            
+        }
+    }
+}
